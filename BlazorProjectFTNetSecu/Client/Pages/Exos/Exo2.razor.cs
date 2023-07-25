da670522583711b610c97579594d0b8f58c6fc4f
BS-GavinC <98208007+VZGLY@users.noreply.github.com>
2023-07-25 11:48:02 +0200

    Exo2 Ended

---
+++ BlazorProjectFTNetSecu/Client/Pages/Exos/Exo2.razor.cs
@@ -0,0 +1,25 @@
+﻿using BlazorProjectFTNetSecu.Shared;
+
+namespace BlazorProjectFTNetSecu.Client.Pages.Exos
+{
+    public partial class Exo2
+    {
+
+        public Exo2()
+        {
+            ExoFormData = new ExoForm();
+        }
+
+        ExoForm ExoFormData { get; set; }
+
+        public bool FormValidated { get; set; } = false;
+
+        void OnValidSubmit()
+        {
+            Console.WriteLine(ExoFormData.Color);
+            FormValidated = true;
+        }
+
+
+    }
+}
---
+++ BlazorProjectFTNetSecu/Shared/ExoForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorProjectFTNetSecu.Shared
+{
+    public class ExoForm
+    {
+        /*
+         * 
+         * Nom EMail Tel Message Color
+         * 
+         */
+
+        [Required]
+        public string Name { get; set; }
+
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required]
+        [MinLength(10)]
+        public string Tel { get; set; }
+
+
+        [Required]
+        public string Message { get; set; }
+
+
+        [Required]
+        [StringLength(7)]
+        public string Color { get; set; }
+
+        
+
+
+    }
+}
